96c4869f391d51b90cc5ee08fb7c5f94b0fdfd97
Nay Thu Rein Soe Tint <naythureign15@gmail.com>
2024-03-11 14:06:30 -0700

    Third commit

--- LMS/app/Http/Controllers/LibraryController.php
+++ LMS/app/Http/Controllers/LibraryController.php
@@ -19,7 +19,7 @@ class LibraryController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth')->except(['index']);
+        $this->middleware('auth')->except(['index', 'approval']);
     }
 
     public function index() {
